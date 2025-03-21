1eb603f38f2652c3bc1e8ff499ce0fc58b6daa51
Matetiga <martinnajera@me.com>
2025-03-21 19:39:38 +0100

    Added background and player movement while attacking

--- mygame/scripts/player.gd
+++ mygame/scripts/player.gd
@@ -5,7 +5,7 @@ const SPEED = 150.0
 const JUMP_VELOCITY = -230.0
 @onready var player_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @export var is_attacking = false
-@onready var animation_player: AnimationPlayer = $SwipeAttack/attack/AnimationPlayer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 @onready var collision_attack: CollisionShape2D = $SwipeAttack/attack/CollisionShape2D
 @onready var sprite_attack: Sprite2D = $SwipeAttack/Sprite2D
@@ -23,39 +23,48 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("attack") and not is_attacking:
 		attack()
 
+	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction < 0:
-		player_sprite.flip_h = true
-		#sprite_attack.flip_h = true
-		#sprite_attack.position.x = -17
-		#collision_attack.position.x = -29
-	elif direction > 0:
-		player_sprite.flip_h = false
-		#sprite_attack.flip_h = false
-		#sprite_attack.position.x = 17
-		#collision_attack.position.x = 29
+	if not is_attacking:
+		if direction < 0:
+			player_sprite.flip_h = true
+			sprite_attack.flip_h = true
+			sprite_attack.position.x = -17
+			collision_attack.position.x = -29
+		elif direction > 0:
+			player_sprite.flip_h = false
+			sprite_attack.flip_h = false
+			sprite_attack.position.x = 17
+			collision_attack.position.x = 29
+	
+	# Player Movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
+	# Animations
 	if direction == 0:
 		player_sprite.play("idle")
 	else:
 		player_sprite.play("run")
+
 	move_and_slide()
 
 
 func attack():
-	animation_player.play("attack")
 	is_attacking = true
-	await animation_player.animation_finished  # Wait for animation to finish
+	animation_player.play("attack")
+	# Overlapping_areas works better than "body_entered" (area's signal)
+	var overlapping_object = $SwipeAttack/attack.get_overlapping_areas()
+	for object in overlapping_object:
+		var parent = object.get_parent()
+		if parent is Enemy:
+			print(parent.name)
+			parent.queue_free()
+	
+	# if is_attacking's value changed inside the Animation tab, bug more likely to happen
+	await animation_player.animation_finished
 	is_attacking = false
-
-
-func _on_attack_body_entered(body: Node2D) -> void:
-	if body is Enemy:
-		print("enemy hit")
-		body.queue_free()
+	print("attack done")
